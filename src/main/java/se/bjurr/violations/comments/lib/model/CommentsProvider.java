94b4158428e56e84db30438364092421535e67ef
Patrizio Bonzani <bonzani@users.noreply.github.com>
2018-09-15 21:34:52 +0200

    First approach of a better named API with more documentation.

--- src/main/java/se/bjurr/violations/comments/lib/model/CommentsProvider.java
+++ src/main/java/se/bjurr/violations/comments/lib/model/CommentsProvider.java
@@ -1,26 +1,60 @@
 package se.bjurr.violations.comments.lib.model;
 
 import java.util.List;
+
 import se.bjurr.violations.lib.util.Optional;
 
+/**
+ * Callback interface for applications that supply the comments to a consumer.
+ * The implementation of it defines which kind of callback should be made.
+ *
+ * @author Patrizio Bonzani
+ */
 public interface CommentsProvider {
-  void createCommentWithAllSingleFileComments(String string);
 
-  void createSingleFileComment(ChangedFile file, Integer line, String comment);
+	/**
+	 * Create a single comment containing all file comments and violations.
+	 *
+	 * @param string The combined comments.
+	 */
+	void createBulkComment(String string);
 
-  List<Comment> getComments();
+	/**
+	 * Create a discussion on the diff on the line the violation occurred.
+	 *
+	 * @param file The file in which the violation occurred.
+	 * @param content The text of the comment.
+	 * @param newLine The line number after the patch.
+	 * @param oldLine The line number before the patch.
+	 */
+	void createDiffComment(ChangedFile file, String content, Integer newLine,
+			Integer oldLine);
 
-  List<ChangedFile> getFiles();
+	List<Comment> getComments();
 
-  void removeComments(List<Comment> comments);
+	List<ChangedFile> getFiles();
 
-  boolean shouldComment(ChangedFile changedFile, Integer line);
+	void removeComments(List<Comment> comments);
 
-  boolean shouldCreateCommentWithAllSingleFileComments();
+	boolean shouldComment(ChangedFile changedFile, Integer line);
 
-  boolean shouldCreateSingleFileComment();
+	/**
+	 * Returns if comments and violations should be combined into one comment.
+	 *
+	 * @return <code>true</code> if the comments should be combined,
+	 *         <code>false</code> otherwise.
+	 */
+	boolean shouldCreateBulkComment();
 
-  boolean shouldKeepOldComments();
+	/**
+	 * Returns if a comment per violation should be made on the diff.
+	 *
+	 * @return <code>true</code> if each comment containing the violation
+	 *         should be posted on the diff, <code>false</code> otherwise.
+	 */
+	boolean shouldCreateCommentPerViolation();
 
-  Optional<String> findCommentTemplate();
+	boolean shouldKeepOldComments();
+
+	Optional<String> findCommentTemplate();
 }
